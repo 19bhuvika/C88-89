74c171f106fa6d9be12b0d8568adea8e2d91dc32
19bhuvika <91776648+19bhuvika@users.noreply.github.com>
2022-11-05 13:48:41 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function adduser()
+{
+player1_name=document.getElementById("player1_name_input").value;
+player2_name=document.getElementById("player2_name_input").value;
+
+LocalStorage.setItem("player1_name",player1_name);
+LocalStorage.setItem("player2_name",player2_name);
+}
